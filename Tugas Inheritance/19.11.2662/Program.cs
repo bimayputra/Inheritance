bbeb922a2713704fd9e5e02ba3344547f45b3bcb
bimayputra <bimay.p@students.amikom.ac.id>
2020-04-11 14:41:29 +0700

    Inherintance

---
+++ Tugas Inheritance/19.11.2662/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LatihanInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Hari", 18);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Laksono", 33, 167806, "Guru Fisika");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Zahwa", 19, 19680, "Zahwanawa@gmail.com");
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+
+        }
+    }
+}
